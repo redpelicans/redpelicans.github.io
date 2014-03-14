6754e17a5bedd97ab55b00a7ecfc0577e211ef53
nicolas-briemant <nicolas.briemant@gmail.com>
2014-03-14 11:38:45 +0100

    rm keepalive from connect (because of watch task)

--- grunt/connect.js
+++ grunt/connect.js
@@ -6,7 +6,6 @@ module.exports = {
       port: process.env.PORT || 9000
     , hostname: process.env.HOST || 'localhost'
     , livereload: 35729 // livereload port
-    , keepalive: true
     }
   }
 };
