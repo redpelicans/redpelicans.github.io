5f79bf1de55d7c5d8f80a3d28b47ed96ecc9b0a8
nicolas-briemant <nicolas.briemant@gmail.com>
2014-03-17 12:22:21 +0100

    set debug to true for grunt connect task

--- grunt/connect.js
+++ grunt/connect.js
@@ -6,6 +6,7 @@ module.exports = {
       port: process.env.PORT || 9000
     , hostname: process.env.HOST || 'localhost'
     , livereload: 35729 // livereload port
+    , debug: true
     }
   }
 };
